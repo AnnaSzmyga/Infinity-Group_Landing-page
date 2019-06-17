5dd0b0f21848ec48cd7064aa4d5d21300860c916
Anna Szmyga <anna.szmyga@o2.pl>
2019-06-17 09:31:09 +0200

    Add sections numbers and 'jump-to' buttons

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
-// Mobile navigation
+
+// MOBILE NAVIGATION
 
 const mobileMenuButton = document.querySelector('.mobile-menu-btn');
 const hamburgerIcon = document.querySelector('.hamburger-icon');
@@ -13,7 +14,7 @@ hamburgerIcon.addEventListener('click', showMenu);
 
 
 
-// Cookies closing
+// CLOSE COOKIES
 
 const cookies = document.querySelector('.cookies');
 const cookiesButton = document.querySelector('.cookies-btn');
@@ -25,10 +26,28 @@ closeCookies = () => {
 cookiesButton.addEventListener('click', closeCookies);
 
 
-//Carousel
+// CAROUSEL
 
 const flkty = new Flickity( '.carousel', {
-  wrapAround: true,
-  autoPlay: true
+    wrapAround: true,
+    autoPlay: true
 });
+
+
+// JUMP TO
+
+//Show Jump-to buttons on scroll:
+
+window.onscroll = function() {showJumpToOnScroll()};
+
+showJumpToOnScroll = () => {
+    const headerHeight = document.querySelector('.header').offsetHeight;
+
+    if (document.body.scrollTop > headerHeight || document.documentElement.scrollTop > headerHeight) {
+        document.querySelector('.jump-to').style.display = "block";
+    } else {
+        document.querySelector('.jump-to').style.display = "none";
+    }
+};
+
 
