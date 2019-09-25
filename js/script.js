1f64b876a4945beb1b5926da0967cffec0a15114
Anna Szmyga <anna.szmyga@o2.pl>
2019-09-25 13:19:07 +0200

    Add jumping to previous/next section feature

--- js/script.js
+++ js/script.js
@@ -41,14 +41,64 @@ const flkty = new Flickity( '.carousel', {
 window.onscroll = function() {showJumpToOnScroll()};
 
 const headerHeight = document.querySelector('.header').offsetHeight;
-const jumpTo = document.querySelector('.jump-to');
+const jumpToButton = document.querySelector('.jump-to');
 
 showJumpToOnScroll = () => {
     if (document.documentElement.scrollTop > (0.5 * headerHeight)) {
-        jumpTo.style.display = "block";
+        jumpToButton.style.display = "block";
     } else {
-        jumpTo.style.display = "none";
+        jumpToButton.style.display = "none";
     }
 };
+
+
+// Jump-to buttons feature
+
+const sections = document.querySelectorAll('[data-jump-to]');
+const sectionsNames = [...sections].map((section) => section.dataset.jumpTo);
+
+const jumpUpButton = document.querySelector('.jump-up');
+const jumpDownButton = document.querySelector('.jump-down');
+
+let currentSection = 'home';
+let previousSection;
+let nextSection;
+
+const changeHrefs = () => {
+    jumpUpButton.setAttribute("href", `#${previousSection}`);
+    jumpDownButton.setAttribute("href", `#${nextSection}`);
+}
+
+const setCurrentSection = () => {
+    sections.forEach((section) => {
+
+        // check if section is in viewport
+        const sectionCoordinates = section.getBoundingClientRect();
+        const viewportHeight = window.innerHeight || document.documentElement.clientHeight;
+        const { top, bottom } = sectionCoordinates;
+
+        if (
+            (top >= 0) && (bottom < viewportHeight) /* when section hight is less than viewport height */
+            /* when section is higher than viewport */
+            || ((top < 0) && (bottom > 0.5 * viewportHeight) && (bottom < viewportHeight))
+            || ( (top < 100) && (top > 0) && (bottom > viewportHeight) )
+            || ( (top < 0) && (bottom > viewportHeight) )
+        ) {
+            currentSection = section.dataset.jumpTo;
+        }
+    })
+    const currentSectionIndex = sectionsNames.indexOf(currentSection);
+    previousSection = currentSectionIndex > 0 ? sectionsNames[currentSectionIndex - 1] : currentSection;
+    nextSection = currentSectionIndex < (sectionsNames.length - 1) ? sectionsNames[currentSectionIndex + 1] : currentSection;
+}
+const checkSection = () => {
+    setCurrentSection();
+    changeHrefs();
+}
+checkSection();
+
+window.addEventListener('scroll', checkSection);
+
+
 
 
